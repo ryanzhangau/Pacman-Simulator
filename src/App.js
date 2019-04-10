dd3a21ba5bbf659fce6a4e8a9779112397daba77
Ryan Zhang <ryanzhangau@gmail.com>
2019-04-10 22:32:46 +1000

    build app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Field from './Components/Field';
+import Button from './Components/Button';
+import { place, move, turnLeft, turnRight } from './Commands';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: 0,
+      y: 0,
+      f: 'NORTH',
+      placed: false,
+      log: 'I am at (0, 0), facing NORTH'
+    };
+    this.update = this.update.bind(this);
+    this.runCommand = this.runCommand.bind(this);
+  }
+
+  update(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  runCommand(e) {
+    let dir;
+    const { x, y, f } = this.state;
+    switch (e.target.innerHTML) {
+      case 'Place':
+        const placePoint = place(x, y, f);
+        this.setState({
+          x: placePoint.x,
+          y: placePoint.y,
+          log: placePoint.log
+        });
+        break;
+      case 'Move':
+        const m = move(x, y, f);
+        if (typeof m === 'string') this.setState({ log: m });
+        else
+          this.setState({
+            x: m.x,
+            y: m.y,
+            log: m.log
+          });
+        break;
+      case 'Left':
+        dir = turnLeft(x, y, f);
+        this.setState({ f: dir.f, log: dir.log });
+        break;
+      case 'Right':
+        dir = turnRight(x, y, f);
+        this.setState({ f: dir.f, log: dir.log });
+        break;
+      case 'Report':
+        break;
+      default:
+        this.setState({ log: 'Invlid command' });
+        break;
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <Field type='number' label='x' value={this.state.x} update={this.update} />
+        <Field type='number' label='y' value={this.state.y} update={this.update} />
+        <Field type='text' label='f' value={this.state.f} update={this.update} />
+        <Button label='Place' click={this.runCommand} />
+        <Button label='Move' click={this.runCommand} />
+        <Button label='Left' click={this.runCommand} />
+        <Button label='Right' click={this.runCommand} />
+        <Button label='Report' click={this.runCommand} />
+        <div>{this.state.log}</div>
       </div>
     );
   }
