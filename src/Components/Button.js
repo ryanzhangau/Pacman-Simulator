7cd6231928980d4595ac9bb2d23ab99df812633d
Ryan Zhang <ryanzhangau@gmail.com>
2019-04-10 22:32:31 +1000

    create button component

---
+++ src/Components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = props => {
+  const { label, click } = props;
+  return <button onClick={click}>{label}</button>;
+};
+
+Button.propTypes = {
+  label: PropTypes.string.isRequired,
+  click: PropTypes.func.isRequired
+};
+
+export default Button;
